7eca647ab339a2f82080d2fb03c4d3ab7bb531c4
Sohaibkundi2 <sohaibkundi2@gmail.com>
2026-06-30 13:23:03 +0500

    feat: configure EF Core with SQL Server connection

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HealthcareCRM.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // We'll add DbSet properties here as we create models (e.g. Patients, Users)
+    }
+}
